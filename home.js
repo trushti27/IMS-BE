2ddd94a4b96957fdc8468312022ce7ba6e6ea1b1
trushti27 <trushtirupapara27@gmail.com>
2025-06-03 17:18:05 +0530

    ffinal

--- home.js
+++ home.js
@@ -1,32 +1,53 @@
-const libExpress=require("express")
-const cors=require("cors")
-const server=libExpress()
+const { MongoClient } = require('mongodb');
+const libExpress = require("express")
+const cors = require("cors")
+const server = libExpress()
 server.use(cors())
-server.post("/users",(req,res)=>{
-    console.log("Request accepted")
-    res.send("Account Created.")
+
+server.use(libExpress.json())
+
+const connection = new MongoClient("mongodb://trushti:020523@localhost:27017/ims?authSource=ims")
+
+
+
+
+//signup
+server.post("/users", (req, res) => {
+
+    console.log(req.body)
+
+    if (req.body.name && req.body.email && req.body.password) {
+        connection.connect().then(() => connection.db("ims")).then((db) => db.collection("user")).then((collection) => {
+
+            collection.findOne({ email: req.body.email })
+                .then(result => {
+                    if (result) {
+                       res.json({error:"Already Exist"})
+                    } else {
+                        collection.insertOne(req.body)
+                        res.json({message:"created"})
+                    }
+                })
+                .catch(error => {
+                    console.error("Error finding user:", error);
+                });
+
+
+        })
+    }
+    else
+        res.json("required field is empty")
+})
+
+server.get("/users", (req, res) => {
+
+
+    connection.connect().then(() => connection.db("ims")).
+        then((db) => db.collection("user")).then((collection) => collection.find().toArray()).
+        then((result) => res.json(result)).catch((err) => console.log(err))
 
 })
-server.post("/players",(req,res)=>{
-    console.log("Request accepted of player")
-    res.send("Profile Created.")
 
-})
-server.post("/team",(req,res)=>{
-    console.log("Request accepted of team")
-    res.send("team Created.")
-
-})
-server.get("/users",(req,res)=>{
-    res.json(
-        [   {name: "u1"},
-            {name: "u2"},
-            {name: "u3"},
-            {name: "u4"}
-        ]
-    )
-})
-
-server.listen(8000,()=>{
+server.listen(8000, () => {
     console.log("Server is listing over port 8000");
 })
