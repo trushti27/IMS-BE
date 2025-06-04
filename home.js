f19b0edb88820571a57a2dd4eb177821a4a7d68d
trushti27 <trushtirupapara27@gmail.com>
2025-06-04 16:52:39 +0530

    backfinal

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
 const libExpress = require("express")
+const libRandomString=require("randomstring")
 const cors = require("cors")
 const server = libExpress()
 server.use(cors())
@@ -9,6 +10,33 @@ server.use(libExpress.json())
 const connection = new MongoClient("mongodb://trushti:020523@localhost:27017/ims?authSource=ims")
 
 
+/*server.post("/users", async (req, res) => {
+    console.log("user request")
+    if (req.body.name && req.body.email && req.body.password ) {
+        await connection.connect()
+        const db = await connection.db("ims")
+        const collection = await db.collection("user")
+        const result = await collection.find({ "email": req.body.email }).toArray()
+
+        if (result.length > 0) {
+            res.json("User Already Exist!")
+
+
+        } else {
+            await collection.insertOne({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+               
+            })
+            res.json("USER CREATED SUCCESSFULLY!")
+
+        }
+    }
+    else{
+        res.json("required field is empty")
+    }
+})*/
 
 
 //signup
@@ -16,36 +44,70 @@ server.post("/users", (req, res) => {
 
     console.log(req.body)
 
-    if (req.body.name && req.body.email && req.body.password) {
+    if (req.body.name && req.body.email && req.body.password) 
+    {
         connection.connect().then(() => connection.db("ims")).then((db) => db.collection("user")).then((collection) => {
 
-            collection.findOne({ email: req.body.email })
-                .then(result => {
-                    if (result) {
+            collection.find({ email: req.body.email }).toArray()
+                .then((result)=> {
+                    if (result) 
+                    {
                        res.json({error:"Already Exist"})
-                    } else {
+                    }
+                    else 
+                    {
                         collection.insertOne(req.body)
                         res.json({message:"created"})
                     }
                 })
                 .catch(error => {
                     console.error("Error finding user:", error);
-                });
-
-
+                })
         })
+        connection.close();
     }
     else
         res.json("required field is empty")
 })
 
-server.get("/users", (req, res) => {
+//log-in
+server.post("/token", async(req, res) => {
 
+    if(req.body.email && req.body.password)
+    {
+        console.log(req.body)
+        await connection.connect()
+        const db=await connection.db('ims')
+        const collection= await db.collection('user')
+        const result= await collection.find({"emai": req.body.emai, "password":req.body.password}).toArray()
+        if(result.length>0)
+        {
+            //create token
+            const token=libRandomString.generate(6)
 
-    connection.connect().then(() => connection.db("ims")).
-        then((db) => db.collection("user")).then((collection) => collection.find().toArray()).
-        then((result) => res.json(result)).catch((err) => console.log(err))
+            //register token into db
+            const user=result[0];
+            await collection.updateOne(
+                    {_id: user._id},
+                    {$set: {token: token }}
+            )
 
+            //return token
+            res.status(200).json({token:token});
+        }
+        else
+        {
+            res.status(400).json({err: "inavalid id or password!!"})
+        }
+
+    }
+    else
+    res.status(401).json({err: "missing required field"})
+    
+})
+
+server.get("/user/roles",(req,res)=>{
+    if()
 })
 
 server.listen(8000, () => {
